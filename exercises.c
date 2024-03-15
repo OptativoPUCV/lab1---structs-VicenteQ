d4a855e0a158b845b3fe72c02ace9360443b6f2a
Your Name <you@example.com>
2024-03-15 00:07:35 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -73,7 +73,7 @@ int comparar(const void *a, const void *b){
   return 0;
 }
 void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,int result[]) {
-  int *result = NULL;
+  result = NULL;
   int size = size1 + size2;
     result = (int *)malloc(size * sizeof(int));
   if(result == NULL) exit (EXIT_FAILURE);
