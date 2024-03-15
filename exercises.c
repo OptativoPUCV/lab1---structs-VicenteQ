d57985e8b19ae04bf6acd9efd08083c3c06cae53
Your Name <you@example.com>
2024-03-15 00:38:19 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -67,28 +67,6 @@ arreglos en un tercer arreglo también ordenado de menor a mayor.
 void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,int result[]) {
   int newSize = size1 + size2;
   result = malloc(sizeof(int) * newSize);
-  int i = 0, j = 0, k = 0;
-  while (i < size1 && j < size2){
-    if (arr1[i] < arr2[j]){
-      result[k] = arr1[i];
-      i++;
-    }
-    else{
-      result[k] = arr2[j];
-      j++;
-    }
-  }
-  while (i < size1) {
-      result[k] = arr1[i];
-      i++;
-      k++;
-  }
-
-  while (j < size2) {
-      result[k] = arr2[j];
-      j++;
-      k++;
-  }
 }
 
 /*
